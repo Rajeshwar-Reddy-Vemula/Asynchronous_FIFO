aac691041c32116e2dce7a24e7e1ce8daeac7719
ReddyVemula <reddyvemularajeshwar@gmail.com>
2026-02-25 01:30:03 -0800

    Create assertions_Async_FIFO.sv
    Added few properties for async fifo, and few cover properties

---
+++ assertions.sv
@@ -0,0 +1,49 @@
+// Assertions for the Asynchronous FIFO
+// Need to write the BIND FILE
+// Includes some Invariants, CDC data integrity, pointers and flags assertions
+
+longint wcnt, rcnt;
+always @(posedge wclk) if (write) wcnt++;
+always @(posedge rclk) if (read) rcnt++;
+
+property p_data_integrity;
+  longint cnt;
+  logic [N-1:0] data;
+  @(posedge wclk) disable iff(!rst_n)
+  (write, cnt = wcnt, data = wdata) |=> @(posedge rclk) (read && (rcnt===cnt))[->1] ##0 (rdata === data);         // using local variables and checking the FIFO order, this doesnt have to be a circular buffer like teh RTL
+endproperty
+
+
+property p_no_data_creation
+@(posedge rclk) disable iff (!rst_n)
+  gray2bin(wptr_sync) >= gray2bin(rptr);                        // use the already-synchronised write pointer visible in read domain, WRITE THE gray2bin() function or use the synchronoised binary pointers from RTL
+endproperty
+
+
+property p_gray_code_write
+    @(posedge wclk) disable iff(!rst_n)
+  write |-> $onehot( wptr ^ $past(wptr))                  
+endproperty
+
+property p_gray_code_read
+  @(posedge rclk) disable iff(!rst_n)
+  read |-> $onehot( rptr ^ $past(rptr))
+endproperty
+
+
+property p_full_empty_invariant
+  disable iff(!rst_n)
+  !(full && empty)
+endproperty
+
+
+property p_overflow
+@(posedge wclk) disable iff (!rst_n)                             // FIFO never overflows — can't write when full
+  full |-> !write;
+endproperty
+
+
+property p_underflow
+@(posedge rclk) disable iff (!rst_n)                                  // FIFO never underflows — can't read when empty  
+  empty |-> !read;
+endproperty
